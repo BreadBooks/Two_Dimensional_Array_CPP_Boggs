15f4a4219d2cd74df6ce7e2528c65f6484b38362
Brett B <65505937+BreadBooks@users.noreply.github.com>
2021-11-09 04:39:59 +0000

    Forgot to do commits

--- main.cpp
+++ main.cpp
@@ -7,50 +7,51 @@
 //A program to practice working with multi-dimensional arrays
 
 #include <iostream>
-
+#include <vector>
+#include <string>
 using namespace std;
 
-/*
 
-Research Input data: go to http://www.weather.com (Links to an external site.)
 
-Pick two cities of your choice. Fill in the temperature for the 7 days of this week of both cities. Fill in this text data table, which will be used in your program.
+// Research Input data: go to http://www.weather.com (Links to an external site.)
 
-//City_One = 1, City_Two = 2  Replace City_One and City_Two with the names of your cities
+// Pick two cities of your choice. Fill in the temperature for the 7 days of this week of both cities. Fill in this text data table, which will be used in your program.
+
+// Dallas = 1, Denver = 2;  // Replace Dallas and Denver with the names of your cities
 
 //Data Table
 
 //City X, Day X: Temp
+/*
+City 1, Day 1: 76
 
-City 1, Day 1: TEMP
+City 1, Day 2: 72
 
-City 1, Day 2: TEMP
+City 1, Day 3: 68
 
-City 1, Day 3: TEMP
+City 1, Day 4: 66
 
-City 1, Day 4: TEMP
+City 1, Day 5: 64
 
-City 1, Day 5: TEMP
+City 1, Day 6: 73
 
-City 1, Day 6: TEMP
+City 1, Day 7: 65
 
-City 1, Day 7: TEMP
+City 2, Day 1: 64
 
-City 2, Day 1: TEMP
+City 2, Day 2: 57
 
-City 2, Day 2: TEMP
+City 2, Day 3: 59
 
-City 2, Day 3: TEMP
+City 2, Day 4: 60
 
-City 2, Day 4: TEMP
+City 2, Day 5: 67
 
-City 2, Day 5: TEMP
+City 2, Day 6: 57
 
-City 2, Day 6: TEMP
-
-City 2, Day 7: TEMP
-
+City 2, Day 7: 65
 */
+
 
  
 
@@ -114,26 +115,34 @@ int main()
 
     }
 
-   /*For up to 5 Points Extra Credit
+    
+   //For up to 5 Points Extra Credit
 
-      Expand this program:
 
-      1. Create a one dimensional array, cityOne[7] to store the 7 temperatures of city 1, assume that value 0 represents day 1, 1 represents day 2....
 
-      2. Create a on dimensional array, cityTwo[7] to store the 7 temperatures of city 2 , assume that value 0 represents day 1, 1 represents day 2....
 
-      3. Using a for loop (or nested for loop), feed the data of CityOne[] and CityTwo[] into temperature[i][j], where i is index of the City Number, and j is the index of the temp for the day.
 
-      4. print the results of temperature[i][j]; to the screen.
+    //   1. Create a one dimensional array, cityOne[7] to store the 7 temperatures of city 1, assume that value 0 represents day 1, 1 represents day 2....
+    int cityOne[7] = {76, 72, 68, 66, 64, 73, 65};
+    //   2. Create a on dimensional array, cityTwo[7] to store the 7 temperatures of city 2 , assume that value 0 represents day 1, 1 represents day 2....
+    int cityTwo[7] = {64, 57, 59, 60, 67, 57, 65};
+    //   3. Using a for loop (or nested for loop), feed the data of CityOne[] and CityTwo[] into temperature[i][j], where i is index of the City Number, and j is the index of the temp for the day.   
+    for (int i  =0; i < WEEK; i++){
+          temperature[0][i] = cityOne[i];
+        }
+        
+    for (int i = 0; i < WEEK; i++){
+          temperature[1][i] = cityTwo[i];
+        }
+    //   4. print the results of temperature[i][j]; to the screen.
+    std::vector<std::string> cityName = {"Dallas", "Denver"};
+    cout<<"Dallas & Denver Week Temps\n";
+        
+    for (int i = 0; i < CITY; i++){
+        for(int j = 0; j < WEEK; j++){   
+            cout << cityName[i] << ", Day " << j + 1 << " = " << temperature[i][j] << endl;
+       }
+    }
+  return 0;
+ }
 
-    */
-
- 
-
- 
-
-    return 0;
-
-}
-
-//Source
